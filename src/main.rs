f938960ee0278dbd97c97949f4f0608b4dfee946
shafqat1841 <shafqat1841@gmail.com>
2026-03-30 18:52:32 +0500

    implement initial thread code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::thread;
+
+const THREADS_NUMBER: usize = 4;
+
 fn main() {
-    println!("Hello, world!");
+    let number_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let mut handlers = Vec::new();
+
+    for thread_number in 0..=THREADS_NUMBER {
+        let f = move || {
+            for i in number_array.iter() {
+                println!(
+                    "Thread number is: {} and the number is: {}",
+                    thread_number, i
+                );
+            }
+        };
+        let handle = thread::spawn(f);
+        handlers.push(handle);
+    }
+
+    for handle in handlers {
+        handle.join().unwrap();
+    }
+
+    // println!("{}", number_array);
 }
